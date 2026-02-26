a04c0883eb1b04f7775c5996905579350fbafc78
Yasser Corrales <yasser.corrales@gmail.com>
2026-02-25 23:42:18 -0700

    feat(script) delete unused scripts

--- sh/dump-db.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-set -auo pipefail
-
-pg_dump -h dbod-svt-sw-pgdb -p 6600 -d svt_sw_db_test -U admin -s -F p -E UTF-8 -f "$DBNAME"-schema.sql
-pg_dump -h dbod-svt-sw-pgdb -p 6600 -d svt_sw_db_test -U admin -a -F p -E UTF-8 -f "$DBNAME"-data.sql
