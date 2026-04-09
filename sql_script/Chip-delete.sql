9be2b888f3fdaea60e1b4bff4847e9b0cd7e34ac
Yasser Corrales <yasser.corralemorales@cern.ch>
2026-04-09 21:17:09 +0200

    feat(sh) reset block when delete chips

--- sql_script/Chip-delete.sql
+++ sql_script/Chip-delete.sql
@@ -2,21 +2,18 @@ UPDATE main."Asic"
 SET
   "chipId" = NULL;
 
-delete from main."ChipLocation"
-;
+delete from main."ChipLocation";
 
-delete from main."Block"
-;
+delete from main."Block";
 
-select pg_catalog.setval('main."Block_id_seq"', 1, false)
-;
+select
+  pg_catalog.setval ('main."Block_id_seq"', 1, false);
 
-delete from main."Chip"
-;
+delete from main."Chip";
 
-delete from main."Chip"
-;
+delete from main."Chip";
 
-select pg_catalog.setval('main."Chip_id_seq"', 1, false)
-;
+delete from main."Chip";
 
+select
+  pg_catalog.setval ('main."Chip_id_seq"', 1, false);
