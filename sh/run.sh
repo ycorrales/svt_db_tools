36f43ff9f4c62a21832ed7ce191d69923df975a2
Yasser Corrales <yasser.corralemorales@cern.ch>
2026-04-18 22:00:38 +0200

    yc::chore(sh) update script

--- sh/run.sh
+++ sh/run.sh
@@ -1,15 +1,30 @@
 #! /bin/bash
+set -euo pipefail
 
-case "$1" in
+case "${1:-}" in
 -l | --local | local)
   isLocal=true
   shift
   ;;
 esac
 
-# sh sh/psql.sh ${isLocal:+'--local'} --db svt_sw_db_test --dumpdb
-# sh sh/psql.sh ${isLocal:+'--local'} --db svt_sw_db_test --renamedb svt_sw_db_test_bk
-sh sh/psql.sh ${isLocal:+'--local'} --db svt_sw_db_test --dropdb
-sh sh/psql.sh ${isLocal:+'--local'} --db svt_sw_db_test --createdb
-sh sh/psql.sh ${isLocal:+'--local'} --db svt_sw_db_test --run sql_script/SVT_DB_Tables_SourceOfTruth.sql
-sh sh/psql.sh ${isLocal:+'--local'} --db svt_sw_db_test --run svt_sw_db_test-data.sql
+declare -A DB_MAP
+DB_MAP["main"]='svt_sw_db'
+DB_MAP["dev"]='svt_sw_db_test'
+
+for env in "${!DB_NAME[@]}"; do
+  DB_NAME=${DB_MAP["$env"]}
+  echo "-------Dumping db $env-------"
+  sh sh/psql.sh ${isLocal:+'--local'} --db "${DB_NAME}" --dumpdb
+  echo "-------Renaming db $env-------"
+  sh sh/psql.sh ${isLocal:+'--local'} --db "${DB_NAME}" --renamedb "${DB_NAME}_bk"
+  echo "-------droping db $env-------"
+  sh sh/psql.sh ${isLocal:+'--local'} --db "${DB_NAME}" --dropdb
+  echo "-------creating db $env-------"
+  sh sh/psql.sh ${isLocal:+'--local'} --db "${DB_NAME}" --createdb
+  echo "-------creating table structure for db $env-------"
+  echo "from file: sql_script/SVT_DB_Tables_SourceOfTruth.sql"
+  sh sh/psql.sh ${isLocal:+'--local'} --db "${DB_NAME}" --run sql_script/SVT_DB_Tables_SourceOfTruth.sql
+  echo "-------Restoring data to db $env-------"
+  sh sh/psql.sh ${isLocal:+'--local'} --db "${DB_NAME}" --run "${DB_NAME}"-data.sql
+done
